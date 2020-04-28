fe1e19fdc0e7c9d96c07f350cfb68f3848c748e8
Andres Arciniegas <andres.arciniegas@hotmail.com>
2020-04-28 11:27:16 -0500

    GESICOM 2020 Primer commit

---
+++ resources/views/gigruinv/view.blade.php
@@ -0,0 +1,100 @@
+@extends( 'layouts.layout' )
+
+@section( 'titulo' )
+    Detalle del Grupo de Investigación 
+@endsection
+
+@section('contenido')
+    <h1 class="text-center">Detalle del Grupo de Investigación</h1>
+
+    <br><br>
+
+    <div class="row">
+        <div class="col-sm-5">
+            <h4>Región Plan Nacional de Desarrollo: </h4>
+        </div>
+
+        <div class="col-sm-5">
+            <p class="lead">{{$grupo->giregpnd}}</h4>
+        </div>
+    </div>
+
+    <br>
+
+    <div class="row">
+        <div class="col-sm-5">
+            <h4>Regional: </h4>
+        </div>
+
+        <div class="col-sm-5">
+            <p class="lead">{{$grupo->giregion}}</h4>
+        </div>
+    </div>
+
+    <br>
+
+    <div class="row">
+        <div class="col-sm-5">
+            <h4>Centro de Formación: </h4>
+        </div>
+
+        <div class="col-sm-5">
+            <p class="lead">{{$grupo->gicenfor}}</h4>
+        </div>
+    </div>
+
+    <br>
+
+    <div class="row">
+        <div class="col-sm-5">
+            <h4>Nombre del Grupo de Investigación: </h4>
+        </div>
+
+        <div class="col-sm-5">
+            <p class="lead">{{$grupo->ginombre}}</h4>
+        </div>
+    </div>
+
+    <br>
+
+    <div class="row">
+        <div class="col-sm-5">
+            <h4>Mes de creación: </h4>
+        </div>
+
+        <div class="col-sm-5">
+            <p class="lead">{{$grupo->gimescre}}</h4>
+        </div>
+    </div>
+
+    <br>
+
+    <div class="row">
+        <div class="col-sm-5">
+            <h4>Año de creación: </h4>
+        </div>
+
+        <div class="col-sm-5">
+            <p class="lead">{{$grupo->gianocre}}</h4>
+        </div>
+    </div>
+
+    <br>
+    
+    <div class="row">
+        <div class="col-sm-5">
+            <h4>Código Colciencias: </h4>
+        </div>
+
+        <div class="col-sm-5">
+            <p class="lead">{{$grupo->gicodgru}}</h4>
+        </div>
+    </div>
+
+    <br><br>
+
+    <div class="row">
+        <a href="{{route('gigruinv.index')}}"><button class="btn btn-primary">Regresar</button></a>
+    </div>
+
+@endsection
