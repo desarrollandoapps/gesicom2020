aa1c4422df5a00f72a38663fd7a993f7b6f5239d
Jose Oviedo <joseoviedo@MacBook-Air-de-Jose.local>
2020-08-04 16:40:44 -0500

    validaciones en front

--- resources/views/giponinv/insert.blade.php
+++ resources/views/giponinv/insert.blade.php
@@ -24,25 +24,27 @@
                                         @foreach ($errors->all() as $item)
                                             <li> {{$item}} </li>
                                         @endforeach
-                                    </ul>    
+                                    </ul>
                                 </div>
                                 <br>
                             @endif
-                            <form action="{{route('giponinv.store')}} " method="POST">
+                            <form action="{{route('giponinv.store')}} " method="POST" class="needs-validation" novalidate>
                                 @csrf
                                 @method('POST')
-                        
+
                                 <div class="form-row">
                                     <div class="col">
                                         <div class="form-group">
                                             <label>Regional</label>
-                                            {!! Form::select('piregion', $regionales, null, ['placeholder' => 'Seleccione...', 'class' => 'custom-select form-control', 'id' => 'piregion']) !!}
+                                            {!! Form::select('piregion', $regionales, null, ['placeholder' => 'Seleccione...', 'class' => 'custom-select form-control', 'id' => 'piregion', 'required']) !!}
+                                            <div class="invalid-feedback">Debe seleccionar la regional</div>
                                         </div>
                                     </div>
                                     <div class="col">
                                         <div class="form-group">
                                             <label>Centro de formación</label>
-                                            {!! Form::select('picenfor', $centros, null, ['placeholder' => 'Seleccione...', 'class' => 'custom-select form-control', 'id' => 'picenfor']) !!}
+                                            {!! Form::select('picenfor', $centros, null, ['placeholder' => 'Seleccione...', 'class' => 'custom-select form-control', 'id' => 'picenfor', 'required']) !!}
+                                            <div class="invalid-feedback">Debe seleccionar el centro de formación</div>
                                         </div>
                                     </div>
                                 </div>
@@ -50,7 +52,8 @@
                                     <div class="col">
                                         <div class="form-group">
                                             <label>Grupo de investigación vinculado</label>
-                                            {!! Form::select('pigruinv', $grupos, null, ['placeholder' => 'Seleccione...', 'class' => 'custom-select form-control', 'id' => 'pigruinv']) !!}
+                                            {!! Form::select('pigruinv', $grupos, null, ['placeholder' => 'Seleccione...', 'class' => 'custom-select form-control', 'id' => 'pigruinv', 'required']) !!}
+                                            <div class="invalid-feedback">Debe seleccionar el grupo de investigación</div>
                                         </div>
                                     </div>
                                 </div>
@@ -58,13 +61,15 @@
                                     <div class="col">
                                         <div class="form-group">
                                             <label>Línea de investigación</label>
-                                            {!! Form::select('pilinvin', $lineas, null, ['placeholder' => 'Seleccione...', 'class' => 'custom-select form-control', 'id' => 'pilinvin']) !!}
+                                            {!! Form::select('pilinvin', $lineas, null, ['placeholder' => 'Seleccione...', 'class' => 'custom-select form-control', 'id' => 'pilinvin', 'required']) !!}
+                                            <div class="invalid-feedback">Debe seleccionar la línea de investigación</div>
                                         </div>
                                     </div>
                                     <div class="col">
                                         <div class="form-group">
                                             <label>Tipo de ponencia</label>
-                                            {!! Form::select('picodtip', $tiposPonencia, null, ['placeholder' => 'Seleccione...', 'class' => 'custom-select form-control', 'id' => 'picodtip']) !!}
+                                            {!! Form::select('picodtip', $tiposPonencia, null, ['placeholder' => 'Seleccione...', 'class' => 'custom-select form-control', 'id' => 'picodtip', 'required']) !!}
+                                            <div class="invalid-feedback">Debe seleccionar el tipo de ponencia</div>
                                         </div>
                                     </div>
                                 </div>
@@ -72,7 +77,8 @@
                                     <div class="col">
                                         <div class="form-group">
                                             <label>Proyecto vinculado</label>
-                                            {!! Form::select('piprovin', $proyectos, null, ['placeholder' => 'Seleccione...', 'class' => 'custom-select form-control', 'id' => 'piprovin']) !!}
+                                            {!! Form::select('piprovin', $proyectos, null, ['placeholder' => 'Seleccione...', 'class' => 'custom-select form-control', 'id' => 'piprovin', 'required']) !!}
+                                            <div class="invalid-feedback">Debe seleccionar el proyecto de investigación vinculado</div>
                                         </div>
                                     </div>
                                 </div>
@@ -80,7 +86,8 @@
                                     <div class="col">
                                         <div class="form-group">
                                             <label>Título de la ponencia</label>
-                                            <input type="text" class="form-control" name="pititulo" id="pititulo" value="{{old('pititulo')}}">
+                                            <input type="text" class="form-control" name="pititulo" id="pititulo" value="{{old('pititulo')}}" required>
+                                            <div class="invalid-feedback">Debe ingresar el título de la ponencia</div>
                                         </div>
                                     </div>
                                 </div>
@@ -88,7 +95,8 @@
                                     <div class="col">
                                         <div class="form-group">
                                             <label>Nombre del evento</label>
-                                            <input type="text" class="form-control" name="pievento" id="pievento" value="{{old('pievento')}}">
+                                            <input type="text" class="form-control" name="pievento" id="pievento" value="{{old('pievento')}}" required>
+                                            <div class="invalid-feedback">Debe ingresar el nombre del evento</div>
                                         </div>
                                     </div>
                                 </div>
@@ -102,7 +110,8 @@
                                                         <i class="far fa-calendar-alt"></i>
                                                     </span>
                                                 </div>
-                                                <input type="text" class="form-control" name="pifecini" value="{{old('pifecini')}}">
+                                                <input type="text" class="form-control" name="pifecini" value="{{old('pifecini')}}" required>
+                                                <div class="invalid-feedback">Debe seleccionar la fecha de inicio del evento</div>
                                             </div>
                                         </div>
                                     </div>
@@ -115,7 +124,8 @@
                                                         <i class="far fa-calendar-alt"></i>
                                                     </span>
                                                 </div>
-                                                <input type="text" class="form-control" name="pifecfin" value="{{old('pifecfin')}}">
+                                                <input type="text" class="form-control" name="pifecfin" value="{{old('pifecfin')}}" required>
+                                                <div class="invalid-feedback">Debe seleccionar la fecha de finzalización del evento</div>
                                             </div>
                                         </div>
                                     </div>
@@ -128,38 +138,41 @@
                                                         <i class="far fa-calendar-alt"></i>
                                                     </span>
                                                 </div>
-                                                <input type="text" class="form-control" name="pifecpon" value="{{old('pifecpon')}}">
+                                                <input type="text" class="form-control" name="pifecpon" value="{{old('pifecpon')}}" required>
+                                                <div class="invalid-feedback">Debe seleccionar la fecha de la presentación de la ponencia</div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                
+
                                 <div class="form-row">
                                     <div class="col">
                                         <div class="form-group">
                                             <label>Ciudad</label>
-                                            <input type="text" class="form-control" name="piciudad" id="pititulo" value="{{old('piciudad')}}">
+                                            <input type="text" class="form-control" name="piciudad" id="piciudad" value="{{old('piciudad')}}" required>
+                                            <div class="invalid-feedback">Debe seleccionar la ciudad del evento</div>
                                         </div>
                                     </div>
                                     <div class="col">
                                         <div class="form-group">
                                             <label>Página web del evento</label>
-                                            <input type="text" class="form-control" name="pipagweb" id="pititulo" value="{{old('pipagweb')}}" placeholder="http://www.evento.com">
+                                            <input type="text" class="form-control" name="pipagweb" id="pipagweb" value="{{old('pipagweb')}}" placeholder="http://www.evento.com">
                                         </div>
                                     </div>
                                     <div class="col">
                                         <div class="form-group">
                                             <label>Ámbito</label>
-                                            <select class="custom-select form-control" name="piambito" id="piambito">
+                                            <select class="custom-select form-control" name="piambito" id="piambito" required>
                                                 <option selected value="">{{__('seleccione')}}</option>
                                                 <option value="Local" {{ old('piambito') == "Local" ? 'selected' : '' }}>Local</option>
                                                 <option value="Nacional" {{ old('piambito') == "Nacional" ? 'selected' : '' }}>Nacional</option>
                                                 <option value="Internacional" {{ old('piambito') == "Internacional" ? 'selected' : '' }}>Internacional</option>
                                             </select>
+                                            <div class="invalid-feedback">Debe seleccionar el ámbito del evento</div>
                                         </div>
                                     </div>
                                 </div>
-                        
+
                                 <br>
                                 <button type="submit" class="btn btn-primary">Guardar</button>
                             </form>
@@ -169,7 +182,7 @@
             </div>
         </div>
     </div>
-    
+
 @endsection
 
 @section('scripts')
@@ -180,7 +193,7 @@
             language: 'es',
             autoclose: true,
         });
-        $('#piregion').change(function(event){            
+        $('#piregion').change(function(event){
             $.get("../centros/" + event.target.value, function(response, centros){
                 $('#picenfor').empty();
                 $('#picenfor').append("<option value=''>Seleccione...</option>");
@@ -190,7 +203,7 @@
                 }
             });
         });
-        $('#picenfor').change(function(event){            
+        $('#picenfor').change(function(event){
             $.get("../grupos/" + event.target.value, function(response, grupos){
                 $('#pigruinv').empty();
                 $('#pigruinv').append("<option value=''>Seleccione...</option>");
@@ -200,7 +213,7 @@
                 }
             });
         });
-        $('#pigruinv').change(function(event){            
+        $('#pigruinv').change(function(event){
             $.get("../semilleros/" + event.target.value, function(response, semilleros){
                 $('#pisemill').empty();
                 for(i = 0; i < response.length; i++)
@@ -217,5 +230,22 @@
                 }
             });
         });
+        (function() {
+            'use strict';
+            window.addEventListener('load', function() {
+                // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                var forms = document.getElementsByClassName('needs-validation');
+                // Loop over them and prevent submission
+                var validation = Array.prototype.filter.call(forms, function(form) {
+                form.addEventListener('submit', function(event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                        }
+                        form.classList.add('was-validated');
+                }, false);
+                });
+            }, false);
+        })();
     </script>
 @endsection
